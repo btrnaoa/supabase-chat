3a3b7ec1c9f9b5b3f7834057cffcaa430ad0b6f8
Bao <61965034+btrnaoa@users.noreply.github.com>
2024-04-22 13:10:45 +1000

    add db

---
+++ lib/schema.ts
@@ -0,0 +1,30 @@
+import { relations } from "drizzle-orm"
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core"
+import { nanoid } from "nanoid"
+
+export const users = pgTable("users", {
+  id: text("id")
+    .$defaultFn(() => nanoid())
+    .primaryKey(),
+  username: text("username").notNull().unique(),
+})
+
+export const usersRelations = relations(users, ({ many }) => ({
+  messages: many(messages),
+}))
+
+export const messages = pgTable("messages", {
+  id: text("id")
+    .$defaultFn(() => nanoid())
+    .primaryKey(),
+  content: text("content"),
+  createdAt: timestamp("created_at").defaultNow(),
+  userId: text("user_id"),
+})
+
+export const messagesRelations = relations(messages, ({ one }) => ({
+  user: one(users, {
+    fields: [messages.userId],
+    references: [users.id],
+  }),
+}))
