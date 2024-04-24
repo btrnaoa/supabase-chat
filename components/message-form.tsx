30d6699a877f09092dde26f0a76005cbadcf3255
Bao <61965034+btrnaoa@users.noreply.github.com>
2024-04-24 10:36:09 +1000

    reset message form

--- components/message-form.tsx
+++ components/message-form.tsx
@@ -1,15 +1,26 @@
+"use client"
+
 import { sendMessage } from "@/lib/actions/messages"
 import { cn } from "@/lib/utils"
 import { ArrowUp } from "lucide-react"
-import { ComponentPropsWithoutRef } from "react"
+import { ComponentPropsWithoutRef, useRef } from "react"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 
 interface MessageFormProps extends ComponentPropsWithoutRef<"form"> {}
 
 export default function MessageForm({ className }: MessageFormProps) {
+  const ref = useRef<HTMLFormElement>(null)
+
   return (
-    <form action={sendMessage} className={cn(className, "flex gap-2")}>
+    <form
+      ref={ref}
+      action={async (formData) => {
+        await sendMessage(formData)
+        ref.current?.reset()
+      }}
+      className={cn(className, "flex gap-2")}
+    >
       <Input type="text" name="message" required />
       <Button type="submit" variant="outline" size="icon" className="flex-none">
         <ArrowUp className="h-4 w-4" />
